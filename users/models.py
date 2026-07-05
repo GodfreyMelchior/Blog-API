bbb4fc90e9c63edfca4272f7a85d269c892770b4
GodfreyMelchior <goddydev@gmail.com>
2026-07-05 17:11:40 +0300

    remove null true in bio

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
+    bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
