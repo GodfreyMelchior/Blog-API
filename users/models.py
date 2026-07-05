7eb6f2906ae00bc4d32659c220b1fcdcdde45fb0
GodfreyMelchior <goddydev@gmail.com>
2026-07-05 17:07:40 +0300

    creating a project, installing dependencies and adding user model

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+
+# Create your models here.
+
